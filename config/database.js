c50bbb9942a5c2ae6d694bce367f74f2422c4cf4
shailendra yadav <30329780+ydvsailendar@users.noreply.github.com>
2018-02-15 15:06:55 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://ydvsailendar0905:standbyme568@ds231588.mlab.com:31588/prodeas-prod'}
+  module.exports = {mongoURI: 'mongodb://<username>:<password>@ds231588.mlab.com:31588/prodeas-prod'}
 }else{
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
